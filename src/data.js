d50d4e6f373e2224d3baf44018cf12e0cd3aeea2
ishaanjain047 <83901051+ishaanjain047@users.noreply.github.com>
2022-01-23 19:03:31 +0530

    first commit proper

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: "civilconclave.iitr.ac.in",
+    subtitle: "MERN Stack and API connectivity",
+    description:
+      "Civil Conclave is the biggest civil engineering event of IIT Roorkee , it is a responsive website which consists of carousel too",
+    image: "./assets/conclavepreview.png",
+    link: "https://civilconclave.iitr.ac.in/",
+  },
+  {
+    title: "shelfpay.in",
+    subtitle: "React JS and CSS",
+    description:
+      "Shelf Pay is an upcoming startup , this is a website which is best example of responsive website",
+    image: "./assets/shelfpreview.png",
+    link: "https://shelfpay.in",
+  },
+  {
+    title: "Login and Register Page",
+    subtitle: "React , Node.js and MONGO DB",
+    description:
+      "Login and Register Page which checks all the details with complete backend server made with Mongo DB ",
+    image: "./assets/loginpreview.png",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://pythonbootcamp.com",
+  },
+];
---
+++ src/dataskills.js
@@ -0,0 +1,20 @@
+export const skills = [
+  {
+    name: "C++",
+  },
+  {
+    name: "Competitive Programming",
+  },
+  {
+    name: "React JS",
+  },
+  {
+    name: "React Native",
+  },
+  {
+    name: "MERN Development",
+  },
+  {
+    name: "Management",
+  },
+];
